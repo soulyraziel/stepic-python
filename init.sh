81c74b9a57af449d84ddfda2142f50d56fbfa944
soulyraziel <soulyraziel@gmail.com>
2016-03-14 13:26:11 +0300

    Init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 rm /etc/nginx/sites-enabled/default
-﻿ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 ln -s /home/box/web/hello.py /etc/gunicorn.d/hello.py
+ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
