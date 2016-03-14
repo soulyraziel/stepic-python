ddb9da12b37f2cd0099f7df788f3f381e5cac842
soulyraziel <soulyraziel@gmail.com>
2016-03-14 15:14:58 +0000

    Django ready

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -4,10 +4,10 @@ admin.autodiscover()
 
 urlpatterns = [
 	url(r'^$', 'qa.views.test'),
-	url(r'^login/.*$', 'qa.views.test'),
-	url(r'^signup/,*$', 'qa.views.test'),
-	url(r'^question/.*/.*$', 'qa.views.test'),
-	url(r'^ask/.*$', 'qa.views.test'),
-	url(r'^popular/.*$', 'qa.views.test'),
-	url(r'^new/.*$', 'qa.views.test'),
+	url(r'^login/.*', 'qa.views.test'),
+	url(r'^signup/,*', 'qa.views.test'),
+	url(r'^question/.*', 'qa.views.test'),
+	url(r'^ask/.*', 'qa.views.test'),
+	url(r'^popular/.*', 'qa.views.test'),
+	url(r'^new/.*', 'qa.views.test'),
 ]
